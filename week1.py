6c8412d0b6fb3eb57a93d6d6434c36d02e2f7bc8
yukti <yukti12jain@gmail.com>
2026-06-23 11:09:59 +0530

    week1- list,dict,sets

---
+++ week1.py
@@ -0,0 +1,27 @@
+#list
+fruits=['apple','banana','cherry','kiwi','mango']
+fruits.append('orange')
+print(fruits)
+fruits.insert(1,'grapes')
+print(fruits)   
+fruits.pop()
+print(fruits)
+fruits.remove('banana')
+print(fruits)
+
+#dictionary
+student={'name':'John','age':10,"marks":90,'city':'New York'}
+print (student['name'])
+student['gender'] = 'Male'
+print(student)
+student['age'] = 11
+print(student)
+student.pop('city')
+print(student)
+
+#sets
+cricket={'rahul','virat','dhoni','rohit'}
+football={'rahul','rohit','messi','ronaldo'}
+print('players who play both:',cricket&football)
+print('players who play either:',cricket|football )
+print('players who play cricket but not football:',cricket-football)
