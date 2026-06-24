789e71ad730a7113d50496c2c051fac3d02475b4
yukti <yukti12jain@gmail.com>
2026-06-24 21:30:07 +0530

    recursion and 1st dsa problem

--- day5.py
+++ day5.py
@@ -17,3 +17,14 @@ def fib(n):
         return fib(n-1)+fib(n-2)
 
 print(fib(10))
+
+#checking duplicates
+def contains_duplicate(nums):
+    seen=set(nums)
+    if len(seen)<len(nums):
+        return True
+    else:
+        return False
+    
+print(contains_duplicate([1,2,3,4,5]))
+print(contains_duplicate([1,2,3,4,5,1]))
