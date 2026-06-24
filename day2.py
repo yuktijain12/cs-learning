7e880cd5b9ad11f1b4b57ad122380fde9d555c4b
yukti <yukti12jain@gmail.com>
2026-06-24 10:08:33 +0530

    functions, args,list comprehensions

---
+++ day2.py
@@ -0,0 +1,23 @@
+#functions
+def greet(name,language='English'):
+    if language == 'English':
+        print('Hello',name)
+    elif language== 'Hindi':
+        print('Namaste',name)
+    elif language=='French':
+        print('Bonjour',name)
+greet('Yukti')
+greet('Yukti','Hindi')
+greet('Yukti','French')
+
+#using args
+def add_all(*args):
+     print('Sum:',sum(args))
+add_all(1,2,3)
+add_all(4,5,6,7,8)
+
+#using list comprehension
+list=[i**2 for i in range (1,11)]
+newlist=[i for i in list if i%2==0]
+print(list)
+print(newlist)
