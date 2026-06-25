1d7c702f9399d4d121caa46f2e5066f1eb61f4cd
yukti <yukti12jain@gmail.com>
2026-06-25 11:06:18 +0530

    two sum problem

--- day5.py
+++ day5.py
@@ -28,3 +28,13 @@ def contains_duplicate(nums):
     
 print(contains_duplicate([1,2,3,4,5]))
 print(contains_duplicate([1,2,3,4,5,1]))
+
+#twosum problem
+d={}
+n=int(input('what is the target number?: '))
+nums=[2,7,11,15]
+for pos,num in enumerate(nums):
+    if n-num in d:
+        print(pos,d[n-num])
+    else:
+        d[num]=pos
